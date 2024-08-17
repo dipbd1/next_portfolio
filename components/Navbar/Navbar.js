5a02560334283599b183cd4a0a1603d65956d910
Dip <dipbd1@gmail.com>
2024-08-17 07:12:32 +0600

    Navbar rework

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,35 +1,35 @@
 import Image from "next/image";
-import "./Navbar.module.css";
 
 export default function Navbar() {
-  return (
-    <div className="splash bg-slate-800/50  flex flex-row flex-wrap">
-      {/* here i will place a fire emoji */}
-      <div>
-        <div className="avatar ml-5 mt-2">
-          <div className="w-24 rounded">
-            <Image
-              src="/fire.gif"
-              alt="Burning my brain"
-              height="50"
-              width="50"
-            ></Image>
-          </div>
+    return (
+        <div className="splash bg-slate-800/50 flex flex-row justify-between p-4">
+            {/* Fire emoji */}
+
+            <div className="flex-shrink-0 self-start">
+                <Image
+                    src="/fire.gif"
+                    alt="Burning my brain"
+                    height="80"
+                    width="80"
+                />
+            </div>
+
+            {/* Navigation items */
+            }
+            <div id="nav-bar" className="flex flex-wrap items-center text-orange-600 mx-4">
+                <div id="home" className="mr-2">
+                    Home
+                </div>
+                <div id="contact" className="mr-2">
+                    Contact
+                </div>
+                <div id="resume" className="mr-2">
+                    Resume
+                </div>
+                <div id="projects" className="mr-2">
+                    Projects
+                </div>
+            </div>
         </div>
-      </div>
-      <div id="nav-bar" className="pt-5 ml-auto mr-20 text-orange-600">
-        <div id="home" className="nav-bar">
-          About{" "}
-        </div>
-        <div id="contact" className="nav-bar">
-          Contact{" "}
-        </div>
-        <div id="resume" className="nav-bar">
-          Resume{" "}
-        </div>
-        <div className="nav-bar">Projects </div>
-      </div>
-      {/* <hr className="hr_underline" /> */}
-    </div>
-  );
+    );
 }
